e2e94366862b3d75193b4e08f1e96ad15a550185
PremanandChowdhury <freudigprem@gmail.com>
2022-01-09 22:40:03 +0530

    Songs List & Selected Song Reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+// Reducers
+const songsReducer = () => {
+  return [
+    { title: 'Dance Meri Rani', duration: '03:43' },
+    { title: 'Panghat', duration: '03:34' },
+    { title: 'Raataan Lambiyan', duration: '03:50' },
+    { title: 'Chaka Chak', duration: '04:31' },
+  ]
+}
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload
+  }
+
+  return selectedSong
+}
