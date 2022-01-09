91cdd9416e5905a7e38ebf2c189f5cf09f382cfd
PremanandChowdhury <freudigprem@gmail.com>
2022-01-09 23:29:30 +0530

    Configuring Connect with mapStateToProps

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -7,4 +7,10 @@ class SongList extends Component {
   }
 }
 
-export default connect()(SongList)
+const mapStateToProps = (state) => {
+  return {
+    songs: state.songs,
+  }
+}
+
+export default connect(mapStateToProps)(SongList)
